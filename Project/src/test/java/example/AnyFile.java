e8347865d9c650dd9ecf966d4e87a1b099471ac3
paulovieiraa <eng.paulovieiraa@gmail.com>
2018-10-16 23:07:47 -0300

    Teste

---
+++ Project/src/test/java/example/AnyFile.java
@@ -0,0 +1,13 @@
+package example;
+
+import org.junit.Test;
+
+import example.base.DefaultBaseServiceTest;
+
+public class AnyFile extends DefaultBaseServiceTest {
+
+    @Test
+    public void a (){
+        System.out.println("Teste");
+    }
+}
--- Project/src/test/java/example/web/api/AnyFile.java
+++
@@ -1,4 +0,0 @@
-package example.web.api;
-
-public class AnyFile {
-}
